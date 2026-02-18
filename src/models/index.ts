1fccc7bd13fbc40df119fc3c02eba30c56ee8838
ihsanbakidogan <ihsanbakidogann@gmail.com>
2026-02-18 07:36:54 +0300

    mongo deleted

--- src/models/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as Project } from './Project';
-export { default as Tool } from './Tool';
-export { default as Post } from './Post';
-export { default as Service } from './Service';
